b085af3f670a80a5b888c852f220a659468dae09
Paul Dreyer <pabloos@icloud.com>
2026-01-18 03:13:20 +0100

    remove unused linux code and dependency libc in bindings

--- bindings/native/linux/usb_driver_linux.h
+++ bindings/native/linux/usb_driver_linux.h
@@ -1,8 +1,2 @@
-#include <linux/usbdevice_fs.h>
-#include <linux/types.h>
-#include <linux/ioctl.h>
-#include <linux/hidraw.h>
-#include <linux/input.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <libusb-1.0/libusb.h>
+#include <sys/ioctl.h>
+#include <errno.h>
